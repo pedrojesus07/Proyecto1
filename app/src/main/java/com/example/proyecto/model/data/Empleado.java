6f9d63d93a282d00c281fee7d4165ccffef35b3d
pedrojesus07 <pedrojesus_95_baza@hotmail.com>
2019-12-03 10:45:39 +0100

    1

--- app/src/main/java/com/example/proyecto/model/data/Empleado.java
+++ app/src/main/java/com/example/proyecto/model/data/Empleado.java
@@ -1,4 +1,42 @@
 package com.example.proyecto.model.data;
 
 public class Empleado {
+    private long id;
+    private String login;
+    private String clave;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", clave='" + clave + '\'' +
+                '}';
+    }
 }
